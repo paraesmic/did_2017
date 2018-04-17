eac52940b2c070d244cf8a465467368e96e46762
EdoardoRaffaele <liverdiseasehc@gmail.com>
2018-04-17 11:57:24 +0200

    pushpush

--- android_app/app/src/main/java/it/polito/did/app_android/MainActivity.java
+++ android_app/app/src/main/java/it/polito/did/app_android/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        myToolbar.inflateMenu(R.menu.mainmenu);
 
         List<Lampada> lista_lampade = new ArrayList<>();
         //
@@ -47,6 +49,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         root.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_update:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 
