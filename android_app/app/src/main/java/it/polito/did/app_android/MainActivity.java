faf43003af0c36ace4786c476bc85c383e56111f
Luca Ferraris <paraesmic@hotmail.it>
2018-07-31 17:11:06 +0200

    colori2

--- android_app/app/src/main/java/it/polito/did/app_android/MainActivity.java
+++ android_app/app/src/main/java/it/polito/did/app_android/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onCreateOptionsMenu(Menu menu) {
             // Inflate the menu; this adds items to the action bar if it is present.
             getMenuInflater().inflate(R.menu.mainmenu, menu);
+
             return true;
         }
 
--- android_app/app/src/main/java/it/polito/did/app_android/MainGridActivity.java
+++ android_app/app/src/main/java/it/polito/did/app_android/MainGridActivity.java
@@ -23,7 +23,7 @@ public class MainGridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_maingrid);
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
